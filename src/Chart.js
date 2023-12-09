5d41b2d9e2272e81f5682ff00af8029ccb7948d7
Riya-Parasar <rparasar@uncc.edu>
2023-12-08 23:00:09 -0500

    Fixed charts

--- src/Chart.js
+++ src/Chart.js
@@ -1,128 +1,14 @@
-// Chart.js
-
-/*import React, { useEffect, useRef } from 'react';
-import Chart from 'chart.js/auto';
-
-const ChartComponent = () => {
-  const barChartRef = useRef(null);
-  const pieChartRef = useRef(null);
-  const lineChartRef = useRef(null);
-
-  useEffect(() => {
-    let barChart = null;
-    let pieChart = null;
-    let lineChart = null;
-
-    const barData = {
-      labels: ['Eat out', 'Rent', 'Grocery', 'Shopping', 'Events', 'Holidays', 'Pets'],
-      datasets: [{
-        label: 'Bar Chart',
-        data: [40, 375, 93, 400, 400, 100, 50],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.5)',
-          'rgba(54, 162, 235, 0.5)',
-          'rgba(255, 206, 86, 0.5)',
-          'rgba(75, 192, 192, 0.5)',
-          'rgba(153, 102, 255, 0.5)',
-          'rgba(255, 159, 64, 0.5)',
-          'rgba(23, 45, 123, 0.5)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-          'rgba(23, 45, 123, 1)',
-        ],
-        borderWidth: 1,
-      }],
-    };
-
-    const pieData = {
-      labels: ['Eat out', 'Rent', 'Grocery', 'Shopping', 'Events', 'Holidays', 'Pets'],
-      datasets: [{
-        label: 'Pie Chart',
-        data: [40, 375, 93, 400, 400, 100, 50],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.5)',
-          'rgba(54, 162, 235, 0.5)',
-          'rgba(255, 206, 86, 0.5)',
-          'rgba(75, 192, 192, 0.5)',
-          'rgba(153, 102, 255, 0.5)',
-          'rgba(255, 159, 64, 0.5)',
-          'rgba(23, 45, 123, 0.5)',
-        ],
-        hoverOffset: 4,
-      }],
-    };
-
-    const lineData = {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-      datasets: [{
-        label: 'Line Chart',
-        data: [65, 59, 80, 81, 56, 55, 40],
-        fill: false,
-        borderColor: 'rgba(75, 192, 192, 1)',
-        tension: 0.1,
-      }],
-    };
-
-    if (barChartRef.current) {
-      barChart = new Chart(barChartRef.current, {
-        type: 'bar',
-        data: barData,
-      });
-    }
-
-    if (pieChartRef.current) {
-      pieChart = new Chart(pieChartRef.current, {
-        type: 'pie',
-        data: pieData,
-      });
-    }
-
-    if (lineChartRef.current) {
-      lineChart = new Chart(lineChartRef.current, {
-        type: 'line',
-        data: lineData,
-      });
-    }
-
-    return () => {
-      if (barChart) {
-        barChart.destroy();
-      }
-      if (pieChart) {
-        pieChart.destroy();
-      }
-      if (lineChart) {
-        lineChart.destroy();
-      }
-    };
-  }, []);
-
-  return (
-    <div>
-      <canvas ref={barChartRef} />
-      <canvas ref={pieChartRef} />
-      <canvas ref={lineChartRef} />
-    </div>
-  );
-};
-
-export default ChartComponent;*/
-
-// Chart.js
-
 import React, { useState, useEffect, useRef } from 'react';
 import Chart from 'chart.js/auto';
 
 const ChartComponent = () => {
   const [barChartData, setBarChartData] = useState([40, 375, 93, 400, 400, 100, 50]);
   const [pieChartData, setPieChartData] = useState([40, 375, 93, 400, 400, 100, 50]);
-  const [lineChartData, setLineChartData] = useState([65, 59, 80, 81, 56, 55, 40]);
+  const [lineChartData, setLineChartData] = useState([65, 59, 80, 81, 56, 55, 40, 20, 21, 22, 23, 24]);
+
+  const [barChartLabels, setBarChartLabels] = useState(['Eat out', 'Rent', 'Grocery', 'Shopping', 'Events', 'Holidays', 'Pets']);
+  const [pieChartLabels, setPieChartLabels] = useState(['Eat out', 'Rent', 'Grocery', 'Shopping', 'Events', 'Holidays', 'Pets']);
+  const [lineChartLabels, setLineChartLabels] = useState(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']);
 
   const barChartRef = useRef(null);
   const pieChartRef = useRef(null);
@@ -134,7 +20,7 @@ const ChartComponent = () => {
     let lineChart = null;
 
     const barData = {
-      labels: ['Eat out', 'Rent', 'Grocery', 'Shopping', 'Events', 'Holidays', 'Pets'],
+      labels: barChartLabels,
       datasets: [{
         label: 'Bar Chart',
         data: barChartData,
@@ -161,7 +47,7 @@ const ChartComponent = () => {
     };
 
     const pieData = {
-      labels: ['Eat out', 'Rent', 'Grocery', 'Shopping', 'Events', 'Holidays', 'Pets'],
+      labels: pieChartLabels,
       datasets: [{
         label: 'Pie Chart',
         data: pieChartData,
@@ -179,7 +65,7 @@ const ChartComponent = () => {
     };
 
     const lineData = {
-      labels: ['Eat out', 'Rent', 'Grocery', 'Shopping', 'Event', 'Holidays', 'Pets'],
+      labels: lineChartLabels,
       datasets: [{
         label: 'Line Chart',
         data: lineChartData,
@@ -221,7 +107,7 @@ const ChartComponent = () => {
         lineChart.destroy();
       }
     };
-  }, [barChartData, pieChartData, lineChartData]);
+  }, [barChartData, pieChartData, lineChartData, barChartLabels, pieChartLabels, lineChartLabels]);
 
   const handleBarDataChange = (e, index) => {
     const newData = [...barChartData];
@@ -241,43 +127,79 @@ const ChartComponent = () => {
     setLineChartData(newData);
   };
 
+  const handleBarLabelChange = (e, index) => {
+    const newLabels = [...barChartLabels];
+    newLabels[index] = e.target.value;
+    setBarChartLabels(newLabels);
+  };
+
+  const handlePieLabelChange = (e, index) => {
+    const newLabels = [...pieChartLabels];
+    newLabels[index] = e.target.value;
+    setPieChartLabels(newLabels);
+  };
+
+  const handleLineLabelChange = (e, index) => {
+    const newLabels = [...lineChartLabels];
+    newLabels[index] = e.target.value;
+    setLineChartLabels(newLabels);
+  };
+
   return (
     <div>
       <div>
         <h3>Bar Chart</h3>
-        {barChartData.map((dataPoint, index) => (
-          <input
-            key={`bar_${index}`}
-            type="number"
-            value={dataPoint}
-            onChange={(e) => handleBarDataChange(e, index)}
-          />
+        {barChartLabels.map((label, index) => (
+          <div key={`bar_label_${index}`}>
+            <input
+              type="text"
+              value={label}
+              onChange={(e) => handleBarLabelChange(e, index)}
+            />
+            <input
+              type="number"
+              value={barChartData[index]}
+              onChange={(e) => handleBarDataChange(e, index)}
+            />
+          </div>
         ))}
       </div>
       <canvas ref={barChartRef} />
 
       <div>
         <h3>Pie Chart</h3>
-        {pieChartData.map((dataPoint, index) => (
-          <input
-            key={`pie_${index}`}
-            type="number"
-            value={dataPoint}
-            onChange={(e) => handlePieDataChange(e, index)}
-          />
+        {pieChartLabels.map((label, index) => (
+          <div key={`pie_label_${index}`}>
+            <input
+              type="text"
+              value={label}
+              onChange={(e) => handlePieLabelChange(e, index)}
+            />
+            <input
+              type="number"
+              value={pieChartData[index]}
+              onChange={(e) => handlePieDataChange(e, index)}
+            />
+          </div>
         ))}
       </div>
       <canvas ref={pieChartRef} />
 
       <div>
         <h3>Line Chart</h3>
-        {lineChartData.map((dataPoint, index) => (
-          <input
-            key={`line_${index}`}
-            type="number"
-            value={dataPoint}
-            onChange={(e) => handleLineDataChange(e, index)}
-          />
+        {lineChartLabels.map((label, index) => (
+          <div key={`line_label_${index}`}>
+            <input
+              type="text"
+              value={label}
+              onChange={(e) => handleLineLabelChange(e, index)}
+            />
+            <input
+              type="number"
+              value={lineChartData[index]}
+              onChange={(e) => handleLineDataChange(e, index)}
+            />
+          </div>
         ))}
       </div>
       <canvas ref={lineChartRef} />
